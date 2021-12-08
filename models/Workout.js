f11356c70bd1025ec7f04044b0d25335a10405cf
unknown <alicebe.phan@gmail.com>
2021-12-08 00:03:47 -0600

    added comment and code to workout.js and index.js

--- models/Workout.js
+++ models/Workout.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
+
+    // this const will have date, exercise which includes name, type, sets, weight, reps, duration and distance
     title: String,
     body: String
   });
