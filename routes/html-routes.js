1231acd45dc2f9175f0a0128a647f2eb843962c5
unknown <alicebe.phan@gmail.com>
2021-12-09 03:22:09 -0600

    fixed bugs in html-routes, was able to run the homepage/dashboard

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,17 +2,21 @@ const router = require('express').Router();
 const path = require('path');
 
 
-app.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname + './public/exercise.html'));
+
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
 });
 
-app.get('/index', (req, res) => {
-  res.sendFile(path.join(__dirname + './public/index.html'));
-});
 
-app.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname + './public/stats.html'));
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
   });
 
+
+  router.get('/', (req, res) => {
+    // because it is the homepage, no need to add index after "/"
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+  
 
 module.exports = router;
